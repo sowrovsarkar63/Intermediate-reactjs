3ce9d72fe4ef35572d813136cc6be98eff3b8b83
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-09-30 14:27:48 +0600

    Basic routing practice completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { Fragment } from "react/cjs/react.production.min";
 import "./App.css";
+import About from "./components/About";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import User from "./components/User";
 
 function App() {
     return (
-        <div className="App">
-            <h1>Practice React Router </h1>
-        </div>
+        <Fragment>
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/user">User</Link>
+                            </li>
+                            <li>
+                                <Link to="/header">Header</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Switch>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route path="/user">
+                            <User />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                        <Route path="header">
+                            <Header />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </Fragment>
     );
 }
 
