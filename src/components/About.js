ac58301e4c57f7765a7af98f41586a77d4c8dbc6
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-10-01 13:21:51 +0600

    practicing react router

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,10 @@ export default function About() {
     return (
         <div>
             <h1>This is about page </h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit,
+                ut.
+            </p>
         </div>
     );
 }
---
+++ src/components/Friends.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react/cjs/react.development";
+
+function Friends(props) {
+    const [users, setusers] = useState([]);
+    return (
+        <div>
+            <h1>This is from friend component </h1>
+        </div>
+    );
+}
+
+export default Friends;
